d80693559e224b41968028a2ce4a6b3e11125fb3
Bri Buffington <bri.kudos@gmail.com>
2019-03-12 17:55:36 -0600

    Updates

---
+++ precache-manifest.1e5fe133251b2fa00a211bfe0d5a0d21.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e433d6caf95ee288e409",
+    "url": "/clickemall/static/js/runtime~main.b112902e.js"
+  },
+  {
+    "revision": "990e385f1c0b8a301612",
+    "url": "/clickemall/static/js/main.b081bc89.chunk.js"
+  },
+  {
+    "revision": "ff661de6195320cfc0eb",
+    "url": "/clickemall/static/js/2.822769df.chunk.js"
+  },
+  {
+    "revision": "990e385f1c0b8a301612",
+    "url": "/clickemall/static/css/main.97cf3479.chunk.css"
+  },
+  {
+    "revision": "2d45de17db03de7a799efacc22d349ff",
+    "url": "/clickemall/index.html"
+  }
+];
